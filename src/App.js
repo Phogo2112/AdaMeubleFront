6350336ec2254ac2c69322b01965ad5d01fc706f
Phogo2112 <sulydain85@gmail.com>
2025-11-03 16:02:05 +0100

    Initial projects front

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/hello")
+      .then(response => response.text())
+      .then(data => setMessage(data))
+      .catch(error => console.error("Erreur fetch API :", error));
+  }, []);
+
+  return (
+    <div>
+      <h1>{message || "Chargement..."}</h1>
+    </div>
+  );
+}
+
+export default App;
