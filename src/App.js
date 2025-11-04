40f3950b975cc09edaa7c028a3521572022bd93f
Phogo2112 <sulydain85@gmail.com>
2025-11-04 15:18:45 +0100

    add User ts and auth service

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
   const [message, setMessage] = useState("");
 
   useEffect(() => {
     fetch("http://localhost:8080/api/hello")
-      .then(response => response.text())
-      .then(data => setMessage(data))
-      .catch(error => console.error("Erreur fetch API :", error));
+      .then((response) => response.text())
+      .then((data) => setMessage(data))
+      .catch((error) => console.error("Erreur fetch API :", error));
   }, []);
 
   return (
