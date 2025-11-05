6fb58556f747722764d0bd5f3d572097d462b5d8
Dain <sulydain85@gmail.com>
2025-11-05 10:07:28 +0100

    feat: ajout de l'axios config + authentification

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-import { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
   const [message, setMessage] = useState("");
 
   useEffect(() => {
     fetch("http://localhost:8080/api/hello")
-      .then(response => response.text())
-      .then(data => setMessage(data))
-      .catch(error => console.error("Erreur fetch API :", error));
+      .then((response) => response.text())
+      .then((data) => setMessage(data))
+      .catch((error) => console.error("Erreur fetch API :", error));
   }, []);
 
-  return (
-    <div>
-      <h1>{message || "Chargement..."}</h1>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
